16ca2cce36b8c2e083a5dd296b07582d12550767
Rxyanlaw <a4804260513@gmail.com>
2020-11-30 22:08:44 -0500

    Update Settings.lua

--- Settings.lua
+++ Settings.lua
@@ -1,1 +1,5 @@
-
+local Settings = game.ServerScriptStorage.EngiAntiCheat.Main.Settings
+game:GetService("ReplicatedStorage")
+if game.(path.to.EngiAntiCheat).Directory == Workspace then
+  print("EngiAntiCheat is somehow not running on ServerSccriptStorage!")
+  return "Settings"
